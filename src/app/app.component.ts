a1bf291d010715e61221a3994b9ce6bd7cb625e6
Glaydyesther <glaycruz7@gmail.com>
2022-08-25 19:56:53 -0500

    programa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'programa-angular';
+  datos: Array<any>;
+  fechaInicial: any;
+  info: any;
+  art: any;
+  existInit:any;
+  entrada:any;
+  salida: any;
+  existFinal: any;
+
+  tipo: string = 'cerrado';
+ 
+
+  constructor(){
+    this.datos = [
+      {articulo:1 , fecha: '2021-02-10', mov: 1, cantidad:20},
+      {articulo:2 , fecha: '2021-04-12', mov: 2, cantidad:100},
+      {articulo:3 , fecha: '2021-02-13', mov: 1, cantidad:30},
+      {articulo:4 , fecha: '2021-03-14', mov: 2, cantidad:40},
+      {articulo:5 , fecha: '2021-10-15', mov: 1, cantidad:50},
+      {articulo:6 , fecha: '2021-07-16', mov: 2, cantidad:60},
+      {articulo:7 , fecha: '2021-08-20', mov: 2, cantidad:70},
+    ];
+ 
+  }
+  ngOnInit(): void {
+      this.ver(); 
+  }
+  ver(){
+    this.datos.forEach(element => {
+      if('2021-05-05' > element.fecha){
+      this.art = element.articulo;
+      this.existInit=element.cantidad - element.cantidad;     
+      }
+     
+      
+    });
+  }
+  SendDataonChangeFinal(event: any){
+    
+   
+
+
+  }
+  SendDataonChangeInicial(event: any){
+   this.fechaInicial = event.target.value;    
+    console.log(this.fechaInicial);
+    this.datos.forEach(element => {
+      if('2021-05-05' > element.fecha){
+      }
+     
+      
+    });
+   
+    
+  }
 }
---
+++ src/app/data.ts
@@ -0,0 +1,9 @@
+const datos =[
+    {articulo:1 , fecha: 12/2/21, mov: 1, cantidad:20},
+    {articulo:2 , fecha: 10/4/21, mov: 2, cantidad:100},
+    {articulo:3 , fecha: 5/2/21, mov: 1, cantidad:30},
+    {articulo:1 , fecha: 1/3/21, mov: 2, cantidad:40},
+    {articulo:1 , fecha: 7/10/21, mov: 1, cantidad:50},
+    {articulo:1 , fecha: 9/7/21, mov: 2, cantidad:60},
+    {articulo:1 , fecha: 10/8/21, mov: 2, cantidad:70},
+  ];
